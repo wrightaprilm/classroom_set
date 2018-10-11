2a11970e730201f244d016af0c86986f4e618323
April Wright <wright.aprilm@gmail.com>
2018-10-11 19:52:28 +0000

    Removed hardcoding

--- scripts/importer.py
+++ scripts/importer.py
@@ -1,8 +1,10 @@
 import dendropy
+import sys
+
 
 amphib = dendropy.DnaCharacterMatrix.get(
-    path="../data/plethodon.phy",
-    schema="phylip"
+    path=sys.argv[1],
+    schema=sys.argv[2]
 )
 
-amphib.write_to_path("../data/plethodon.fa", schema="fasta")
+amphib.write_to_path(sys.argv[3], schema=sys.argv[4])
