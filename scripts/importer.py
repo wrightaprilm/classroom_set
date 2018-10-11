b3817dd7fde00237578bb7eee1efc411dcbff170
Vlad Dracula <courtney.grigsby@selu.edu>
2018-10-11 19:54:49 +0000

    Removed hardcoding

--- scripts/importer.py
+++ scripts/importer.py
@@ -1,8 +1,14 @@
 import dendropy
+import sys 
 
-amphib = dendropy.DnaCharacterMatrix.get(
-    path="../data/plethodon.phy",
-    schema="phylip"
-)
+def get_file(read_file, schema, write_file):
+    amphib = dendropy.DnaCharacterMatrix.get(
+        read_file = sys.argv[1],
+        schema= sys.argv[2], 
+    )
 
-amphib.write_to_path("../data/plethodon.fa", schema="fasta")
+amphib.write_to_path(sys.argv[3], schema="fasta")
+
+return 
+
+#get_file("../data/" plethodon.phy", "phylip")
